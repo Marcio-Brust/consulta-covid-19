cfc2cd2ada1c0e43095d445a2dab3c3c1769f3ee
Márcio Brust <brustmw21@gmail.com>
2023-01-11 13:39:36 -0300

    inciando projeto

---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  width: 40rem;
+  margin: 0 auto;
+`;
+
+
+/* https://covid19-brazil-api.now.sh/api/report/v1/
+
+
+https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/sp
+
+
+https://covid19-brazil-api.now.sh/api/report/v1/brazil
+
+
+https://covid19-brazil-api.now.sh/api/report/v1/countries */
